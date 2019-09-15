40acd0a2d0980bcf5ad57f9b5577f71a71cc54f4
raixasantos <39340728+raixasantos@users.noreply.github.com>
2019-09-15 18:12:42 -0300

    Quem sou

---
+++ Quem sou Fatos Históricos/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1568581811,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/logo-sheet0.png",
+		"images/jogar-sheet0.png",
+		"images/quadro-sheet0.png",
+		"images/quadro-sheet1.png",
+		"images/quadro-sheet2.png",
+		"images/textocreditos-sheet0.png",
+		"images/fundopreto-sheet0.png",
+		"images/pontos-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
